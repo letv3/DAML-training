97cfef9b2066be5d4b141f226caf689cc8da08b5
Oleksandr Lytvyn <oleksandr.lytvyn@profect.sk>
2022-02-17 14:48:28 +0100

    finished private-nft sample project and docs sample project

---
+++ create-daml-app/daml.js/create-daml-app-0.1.0/lib/index.d.ts
@@ -0,0 +1,3 @@
+import * as User from './User';
+export { User } ;
+export declare const packageId = 'd31a6d4d50ee91d816e3e476a4e92f51aef3481fbcb53e838b9193ef8fef7384';
---
+++ create-daml-app/daml.js/create-daml-app-0.1.0/lib/index.js
@@ -0,0 +1,10 @@
+"use strict";
+/* eslint-disable-next-line no-unused-vars */
+function __export(m) {
+/* eslint-disable-next-line no-prototype-builtins */
+    for (var p in m) if (!exports.hasOwnProperty(p)) exports[p] = m[p];
+}
+Object.defineProperty(exports, "__esModule", { value: true });
+var User = require('./User');
+exports.User = User;
+exports.packageId = 'd31a6d4d50ee91d816e3e476a4e92f51aef3481fbcb53e838b9193ef8fef7384';
